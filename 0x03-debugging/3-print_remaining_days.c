46a290a7a22a7b7125f8651187ca9f7222fbe6a6
lorexy123 <lorettaahialaka@gmail.com>
2022-04-13 13:48:31 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -32,6 +32,8 @@ void print_remaining_days(int month, int day, int year)
 			{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+
+			return (void);
 			}
 			}
 }
