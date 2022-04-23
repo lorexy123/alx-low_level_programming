f8fb4f3ca0d5dc7c9a43af4e4215ec725407fe32
lorexy123 <lorettaahialaka@gmail.com>
2022-04-23 11:41:02 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,28 @@
 #include "main.h"
 /**
- * leet - a function that encodes a string into 1337
- *
- * @s: string input
- *
- * Return: @s
-*/
-
-char *leet(char *s)
+  * leet - translates string to 1337
+  * @string: pointer to string
+  * Return: returns translated string
+  */
+char *leet(char *string)
 {
-  int i, c = 0;
-  int sl[] = {97, 101, 111, 116, 108};
-  int ul[] = {65, 69, 79, 84, 76};
-  int n[] = {52, 51, 48, 55, 49};
+	int a;
+	int b;
 
-  /*iterate values in array s*/
-  while (s[c] != '\0')
-  {
-    /*
-     * loop through array value five times
-     * to check if the value is equal to
-     * any small letter in array sl or
-     * upper letter in array ul and if it
-     * is replace it with the value in array n
-    */
-    for (i = 0; i < 5; i++)
-    {
-      if (s[c] == sl[i] || s[c] == ul[i])
-      {
-        s[c] = n[i];
-        break;
-      }
-    }
-    c++;
-  }
-  return (s);
+	char lower[] = "aeot1";
+	char upper[] = "AEOTL";
+	char numbers[] = "43071";
+
+	for (a = 0; string[a] != '\0'; a++)
+	{
+		for (b = 0; lower[b] != '\0' && upper[b] != '\0'; b++)
+		{
+			if (string[a] == lower[b] || string[a] == upper[b])
+					{
+					string[a] = number[b];
+					break;
+					}
+		}
+	}
+	return (string);
 }
