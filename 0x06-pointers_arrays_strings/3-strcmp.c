5a2e45fb5d85c5ccf3526a1d904a69e0d51d2296
lorexy123 <lorettaahialaka@gmail.com>
2022-04-23 09:30:26 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -24,7 +24,7 @@ int _strcmp(char *s1, char *s2)
   */
   while (s1[i] != '\0' && s2[i] != '\0')
   {
-    /**
+    /*
      * if they differ by a single character
      * don't iterate further
     */
