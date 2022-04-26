8903826b0a8a8f35814481684f70830082b3f139
lorexy123 <lorettaahialaka@gmail.com>
2022-04-26 12:28:14 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
   * _strspn - print the consecutive characters of s1 that are in s2
- * @s: source string
- * @accept: searching string
- * Return: new string
- */
+  * @s: source string
+  * @accept: searching string
+  * Return: new string
+  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
