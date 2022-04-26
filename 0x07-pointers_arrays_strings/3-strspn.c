014f82d3900a98e6d8fd94d756db284871e525b2
lorexy123 <lorettaahialaka@gmail.com>
2022-04-26 13:20:36 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,23 +1,30 @@
 #include "main.h"
+
 /**
-  * _strspn - print the consecutive characters of s1 that are in s2
+  * _strspn -search a string for a set of bytes
   * @s: source string
-  * @accept: searching string
-  * Return: new string
+  * @accept: accepted string
+  *
+  * Return: number of bytes in the init segment
   */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j;
-	for (j = 0; *(s + j); j++)
-	{
-		for (i = 0; *(accept + i); i++)
-		{
-			if (*(s + j) == *(accept + i))
-				break;
-		}
-		if (*(accept + i) == '\0')
-			break;
-	}
+	unsigned int a = 0, b, t = 0;
 
-	return (j);
+	while (accept[a])
+			{
+			b = 0;
+
+			while (s[b] != 32)
+			{
+				if (accept[a] == s[b])
+				{
+					t++;
+				}
+
+				b++;
+			}
+			a++;
+			}
+	return (t);
 }
